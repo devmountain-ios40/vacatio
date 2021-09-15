5cfdce2cbd7670006e85ebe54a0a1ca2ab243053
Stephen A. Davis <stephen@webdavis.io>
2021-09-15 11:04:08 -0600

    Add bare-bones Vacatio Xcode project

---
+++ Vacatio/Vacatio/Controllers/ViewControllers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Vacatio
+//
+//  Created by Stephen Davis on 9/15/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
